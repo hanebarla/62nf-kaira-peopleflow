ebbfae1e92c28946687a91dd3d1d669a99487ed6
hanebarla <barla.wings@gmail.com>
2021-03-25 17:51:57 +0900

    re-define model arch

--- src/model.py
+++ src/model.py
@@ -39,7 +39,8 @@ class ContextualModule(nn.Module):
 
     def forward(self, feats):
         h, w = feats.size(2), feats.size(3)
-        multi_scales = [F.upsample(input=stage(feats), size=(h, w), mode='bilinear') for stage in self.scales]
+        # multi_scales = [F.upsample(input=stage(feats), size=(h, w), mode='bilinear') for stage in self.scales]
+        multi_scales = [F.interpolate(input=stage(feats), size=(h, w), mode='bilinear') for stage in self.scales]
         weights = [self.__make_weight(feats,scale_feature) for scale_feature in multi_scales]
         overall_features = [(multi_scales[0]*weights[0]+multi_scales[1]*weights[1]+multi_scales[2]*weights[2]+multi_scales[3]*weights[3])/(weights[0]+weights[1]+weights[2]+weights[3])]+ [feats]
         bottle = self.bottleneck(torch.cat(overall_features, 1))
--- src/plot.py
+++ src/plot.py
@@ -21,10 +21,14 @@ transform = torchvision.transforms.Compose([
 model = CANNet2s()
 checkpoint = torch.load('weights/model_best.pth.tar')
 model.load_state_dict(fix_model_state_dict(checkpoint['state_dict']))
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+model.eval()
+print(device)
 
 input_img = {
-    'first': torch.zeros((1, 360, 640)),
-    'second': torch.zeros((1, 360, 640))
+    'first': torch.zeros((3, 360, 640)),
+    'second': torch.zeros((3, 360, 640))
 }
 
 
@@ -35,12 +39,16 @@ def run(img_str):
     decimg = np.array(decimg, dtype=np.uint8)
     decimg = cv2.cvtColor(decimg, cv2.COLOR_BGR2RGB)
     h, w, c = decimg.shape
-    decimg = decimg[(int(h/2)-180):(int(h/2)+180), (int(w/2)-320):(int(w/2)+320)]
-    """
-    input_img['second'] = transform(decimg)
+    re_decimg = decimg[(int(h/2)-180):(int(h/2)+180), (int(w/2)-320):(int(w/2)+320)]
+
+    input_img['second'] = transform(re_decimg)
+
+    prev_img = input_img['first'].to(device).unsqueeze(0)
+    img = input_img['second'].to(device).unsqueeze(0)
 
     with torch.no_grad():
-        pred = model(input_img['first'].cuda(), input_img['second'].cuda())
+        pred = model(prev_img, img)
+        """
         mask_boundry = torch.zeros(pred.shape[2:])
         mask_boundry[0, :] = 1.0
         mask_boundry[-1, :] = 1.0
@@ -52,7 +60,8 @@ def run(img_str):
             F.pad(pred[0, 5, :, :-1], (1, 0, 0, 0)) + F.pad(pred[0, 6, :-1, 1:], (0, 1, 1, 0)) + \
             F.pad(pred[0, 7, :-1, :], (0, 0, 1, 0)) + F.pad(pred[0, 8, :-1, :-1], (1, 0, 1, 0)) + \
             pred[0, 9, :, :] * mask_boundry
-
+    """
+    """
     dense_num = density.to('cpu').detach().numpy().copy()
     dense = cv2.resize(dense_num, (640, 360), interpolation=cv2.INTER_CUBIC) / 64.0
     dense *= 255.0
@@ -63,12 +72,12 @@ def run(img_str):
     plot_img[:, :, 2] = cmdense[:, :, 0]
     plot_img *= 255
     """
-    plot_img = np.pad(decimg, (h-decimg.shape[0], w-decimg.shape[1]), 'constant')
+    # plot_img = np.pad(re_decimg, (h-re_decimg.shape[0], w-re_decimg.shape[1]), 'constant')
 
     # fig, ax = plt.subplots()
 
     # encode to string
-    _, encimg = cv2.imencode(".jpg", plot_img, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
+    _, encimg = cv2.imencode(".jpg", re_decimg, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
     img_str = encimg.tostring()
     img_str = "data:image/jpeg;base64," + base64.b64encode(img_str).decode('utf-8')
     return IPython.display.JSON({'img_str': img_str})
