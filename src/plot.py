7696fbf0a8ab73e76945054e7fb831cc1cab4b00
hanebarla <barla.wings@gmail.com>
2021-03-25 15:42:20 +0900

    create plot

--- src/plot.py
+++ src/plot.py
@@ -1,27 +1,70 @@
+from model import CANNet2s
+
 import IPython
 import cv2
 import numpy as np
 from PIL import Image
 from io import BytesIO
 import base64
+import torch
+import torch.nn.functional as F
+import torchvision
+from matplotlib import cm
+
+
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+model = CANNet2s()
+checkpoint = torch.load('../weights/model_best.pth.tar')
+model.load_state_dict(checkpoint['state_dict'])
+
+input_img = {
+    'first': torch.zeros((1, 360, 640)),
+    'second': torch.zeros((1, 360, 640))
+}
 
 
 def run(img_str):
-    #decode to image
+    # decode to image
     decimg = base64.b64decode(img_str.split(',')[1], validate=True)
     decimg = Image.open(BytesIO(decimg))
-    decimg = np.array(decimg, dtype=np.uint8);
+    decimg = np.array(decimg, dtype=np.uint8)
     decimg = cv2.cvtColor(decimg, cv2.COLOR_BGR2RGB)
+    h, w, c = decimg.shape
+    decimg = decimg[(int(h/2)-180):(int(h/2)+180), (int(w/2)-320):(int(w/2)+320)]
+    input_img['second'] = transform(decimg)
 
     #############your process###############
 
-    out_img = cv2.Canny(decimg,100,200)
-    #out_img = decimg
+    with torch.no_grad():
+        pred = model(input_img['first'].cuda(), input_img['second'].cuda())
+        mask_boundry = torch.zeros(pred.shape[2:])
+        mask_boundry[0, :] = 1.0
+        mask_boundry[-1, :] = 1.0
+        mask_boundry[:, 0] = 1.0
+        mask_boundry[:, -1] = 1.0
+        density = F.pad(pred[0, 0, 1:, 1:], (0, 1, 0, 1)) + \
+            F.pad(pred[0, 1, 1:, :], (0, 0, 0, 1)) + F.pad(pred[0, 2, 1:, :-1], (1, 0, 0, 1)) + \
+            F.pad(pred[0, 3, :, 1:], (0, 1, 0, 0)) + pred[0, 4, :, :] + \
+            F.pad(pred[0, 5, :, :-1], (1, 0, 0, 0)) + F.pad(pred[0, 6, :-1, 1:], (0, 1, 1, 0)) + \
+            F.pad(pred[0, 7, :-1, :], (0, 0, 1, 0)) + F.pad(pred[0, 8, :-1, :-1], (1, 0, 1, 0)) + \
+            pred[0, 9, :, :] * mask_boundry
 
+    dense_num = density.to('cpu').detach().numpy().copy()
+    dense = cv2.resize(dense_num, (640, 360), interpolation=cv2.INTER_CUBIC) / 64.0
+    dense *= 255.0
+    cmdense = cm.jet(dense)[:, :, 0:3]
+    plot_img = cmdense
+
+    plot_img[:, :, 0] = cmdense[:, :, 2]
+    plot_img[:, :, 2] = cmdense[:, :, 0]
+    plot_img *= 255
     #############your process###############
 
     #encode to string
-    _, encimg = cv2.imencode(".jpg", out_img, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
+    _, encimg = cv2.imencode(".jpg", plot_img, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
     img_str = encimg.tostring()
     img_str = "data:image/jpeg;base64," + base64.b64encode(img_str).decode('utf-8')
     return IPython.display.JSON({'img_str': img_str})
